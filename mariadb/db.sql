2f70466a066bbca3d498895dd60f9c707c07646b
Bheesham Persaud <bheesham.persaud@live.ca>
2015-08-03 01:39:03 -0400

    Got Postfix and Dovecot playing nicely.

--- mariadb/db.sql
+++ mariadb/db.sql
@@ -5,42 +5,44 @@ CREATE DATABASE IF NOT EXISTS mail
 
 USE mail;
 
+DROP TABLE IF EXISTS domains;
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS aliases;
+
 CREATE TABLE IF NOT EXISTS domains (
   id BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
   domain VARCHAR(50) NOT NULL,
-  relay TINYINT(1) NOT NULL,
   PRIMARY KEY (id),
   UNIQUE(domain)
-);
+) DEFAULT CHARSET=utf8;
 
--- We don't want to set uid, gid, home, or mail
--- settings from the database. Let's just keep
--- that stuff in Dovecot's configuration.
 CREATE TABLE IF NOT EXISTS users (
-  id BIGINT NOT NULL AUTO_INCREMENT,
+  id BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
+  domain BIGINT UNSIGNED REFERENCES domains(id),
   email VARCHAR(128) NOT NULL,
-  domain BIGINT REFERENCES domains(id),
   password VARCHAR(128) NOT NULL,
   PRIMARY KEY (id),
   UNIQUE (email)
-);
+) DEFAULT CHARSET=utf8;
 
 CREATE TABLE IF NOT EXISTS aliases (
-  id BIGINT NOT NULL AUTO_INCREMENT,
-  alias VARCHAR(128) NOT NULL,
-  email VARCHAR(128) REFERENCES users(email),
+  id BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
+  domain BIGINT UNSIGNED REFERENCES domains(id),
+  source VARCHAR(128) REFERENCES users(email),
+  dest VARCHAR(128) NOT NULL,
   PRIMARY KEY (id),
-  UNIQUE (alias)
-);
+  UNIQUE (source)
+) DEFAULT CHARSET=utf8;
 
-TRUNCATE domains;
-TRUNCATE users;
-TRUNCATE aliases;
+INSERT INTO domains (domain) VALUES
+  ('waffles4.life'),
+  ('fedora22-ser-2'),
+  ('localhost.waffles4.life');
 
-INSERT INTO domains (domain, relay) VALUES ('192.168.122.86', 0);
-INSERT INTO domains (domain, relay) VALUES ('localhost', 0);
-INSERT INTO users (email, domain, password) VALUES
-  ('bhee@192.168.122.86', 1, '{SHA512-CRYPT}$6$LBshVwTIbK7cUCL9$EUhZziTcz1p1duvEbNkLGU3vNXHGJ7Qc3kvepXAOWt6qQ9nMl/tsfVQ8bqhoOJK2KLQU3kIB2mB5.W2wJ8XU11');
-INSERT INTO aliases (alias, email) VALUES ('test@localhost', 'bhee@192.168.122.86');
+INSERT INTO users (domain, email, password) VALUES
+  (1, 'bheesham@waffles4.life', '{SHA512-CRYPT}$6$LBshVwTIbK7cUCL9$EUhZziTcz1p1duvEbNkLGU3vNXHGJ7Qc3kvepXAOWt6qQ9nMl/tsfVQ8bqhoOJK2KLQU3kIB2mB5.W2wJ8XU11');
+
+INSERT INTO aliases (domain, source, dest) VALUES
+  (1, 'alias@waffles4.life', 'bhee@waffles4.life');
 
 COMMIT;
